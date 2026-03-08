7fef846b0a5e21007c8a9a5ecee4b7ff5cc0b645
yamaryosan <canda3267@gmail.com>
2026-03-08 13:49:48 +0900

    refactor: 検索ウィンドウのみ表示するパスを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<>
+			<p>検索画面は以下から開けます。</p>
+			<p>
+				<Link href="/search">/search</Link>
+			</p>
 			<p>アップロード画面は以下から開けます。</p>
 			<p>
 				<Link href="/upload">/upload</Link>
---
+++ src/app/search/page.tsx
@@ -0,0 +1,10 @@
+import SearchWindow from "../search-window";
+
+export default function SearchPage() {
+	return (
+		<>
+			<h1>検索</h1>
+			<SearchWindow />
+		</>
+	);
+}
