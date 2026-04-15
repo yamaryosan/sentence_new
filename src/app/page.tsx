f303e511893c2fedd6680c0d51a95da4d8740f4e
yamaryosan <canda3267@gmail.com>
2026-04-16 00:11:17 +0900

    feat: ランダムページにリダイレクト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-	return (
-		<>
-			<p>
-				保護されたページを開く場合は、まず
-				<Link href="/login" className="ml-1 underline">
-					/login
-				</Link>
-				からログインしてください。
-			</p>
-			<p>検索画面は以下から開けます。</p>
-			<p>
-				<Link href="/search">/search</Link>
-			</p>
-			<p>アップロード画面は以下から開けます。</p>
-			<p>
-				<Link href="/upload">/upload</Link>
-			</p>
-			<p>閲覧画面は以下から。</p>
-			<Link href="/view">/view</Link>
-			<p>ランダム表示は以下から。</p>
-			<Link href="/random">/random</Link>
-		</>
-	);
+	redirect("/random");
 }
